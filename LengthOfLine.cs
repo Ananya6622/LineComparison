3c86bea26b97c1dc8f8c41f285b38fad266d73f8
Ananya6622 <ananyaetti6622@gmail.com>
2023-09-20 21:46:38 +0530

    added

---
+++ LengthOfLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComaprison
+{
+    abstract class Length
+    {
+        public abstract double Line(double X1, double X2, double Y1, double Y2);
+    }
+    class LengthOfLine : Length
+    {
+        public override double Line(double X1, double X2, double Y1, double Y2)
+        {
+            double lengthofline = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            Console.WriteLine(lengthofline);
+            return lengthofline;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace LineComaprison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LineComparision");
+            LengthOfLine length = new LengthOfLine();
+            double Line1 = length.Line(1, 2, 3, 2);
+            double Line2 = length.Line(1, 2, 4, 6);
+
+            //Lines Equals
+            if (Line1 == Line2)
+                Console.WriteLine("lines are equal");
+            else
+                Console.WriteLine("lines are not equal");
+
+            //compare lines
+            if (Line1.CompareTo(Line2) == 0)
+                Console.WriteLine("Both Line are  equal");
+            else
+                Console.WriteLine("They are not equal");
+        }
+    }
+}
